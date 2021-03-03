01b1275f0c199830d1a0ee43ea2910607c6ad639
hiigoo <43704772+hiigoo@users.noreply.github.com>
2021-03-02 21:27:51 -0300

    Update firebase2.js

--- src/firebase2.js
+++ src/firebase2.js
@@ -1,14 +1,12 @@
 import firebase from 'firebase';	
 
 const firebaseConfig = {	
-    apiKey: "API_KEY",
-    authDomain: "PROJECT_ID.firebaseapp.com",
-    databaseURL: "https://PROJECT_ID.firebaseio.com",
-    projectId: "PROJECT_ID",
-    storageBucket: "PROJECT_ID.appspot.com",
-    messagingSenderId: "SENDER_ID",
-    appId: "APP_ID",
-    measurementId: "G-MEASUREMENT_ID",
+    apiKey: "AIzaSyC7bN-fFO026mCR--H4BMh-3p9akM5iFrQ",
+    authDomain: "carlostreina-5e7ee.firebaseapp.com",
+    projectId: "carlostreina-5e7ee",
+    storageBucket: "carlostreina-5e7ee.appspot.com",
+    messagingSenderId: "387815942871",
+    appId: "1:387815942871:web:5a8b2e6b567630ede69e92",
 };	
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);	
